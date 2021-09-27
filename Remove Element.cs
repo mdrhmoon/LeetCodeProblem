1b6c0c2397fc8b996075d3a9fe3091af380cbd01
mdrhmoon <mdrhmoon@gmail.com>
2021-09-28 03:01:13 +0600

    remove element

---
+++ Remove Element.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution obj = new Solution();
+            int[] nums = new int[] { 3, 3 };
+            int val = 5;
+
+            Console.WriteLine(obj.RemoveElement(nums, val));
+        }
+    }
+
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int len = nums.Length;
+            if (len == 0) return len;
+
+            int firstPointer = 0;
+            bool isFirstPointer = false;
+
+            for (int i = 0; i < len; i++)
+            {
+                if(nums[i] == val && !isFirstPointer)
+                {
+                    firstPointer = i-1;
+                    isFirstPointer = true;
+                }
+
+                if(nums[i] != val && isFirstPointer)
+                {
+
+                    firstPointer++;
+                    nums[firstPointer] = nums[i];
+                    continue;
+                }
+            }
+
+            if (isFirstPointer == false) return len;
+
+            return firstPointer + 1;
+        }
+    }
+}
+
