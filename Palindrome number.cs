b00822acc42e56b04db09e578d9475fb8363c01b
mdrhmoon <mdrhmoon@gmail.com>
2021-09-27 22:27:45 +0600

    Palindrome number

---
+++ Palindrome number.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution obj = new Solution();
+            int x = 10;
+
+            Console.WriteLine(obj.IsPalindrome(x));
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+
+            int result = 0; int temp = 0; int number = x;
+            while(number > 0)
+            {
+                result = (result * 10) + number % 10;
+                if (result / 10 != temp) return false;
+                temp = result;
+                number = number / 10;
+            }
+
+            if (result == x) return true;
+
+            return false;
+        }
+    }
+}
+
