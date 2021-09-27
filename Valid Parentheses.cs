81baa6a0a755325e37a18c816dd500b2d11e567f
mdrhmoon <mdrhmoon@gmail.com>
2021-09-28 01:04:45 +0600

    Valid Parentheses

---
+++ Valid Parentheses.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution obj = new Solution();
+            string s = "(){})";
+
+            Console.WriteLine(obj.IsValid(s));
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> parentheses = new Stack<char>();
+
+            char[] charArray = s.ToCharArray();
+            int charArrayLen = charArray.Length;
+            
+            for (int i = 0; i < charArrayLen; i++)
+            {
+                if(charArray[i] == '(' || charArray[i] == '{' || charArray[i] == '[')
+                {
+                    parentheses.Push(charArray[i]);
+                }
+                else
+                {
+                    if (parentheses.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    char closingParentheses = parentheses.Peek();
+
+                    if((closingParentheses == '(' && charArray[i]== ')') || (closingParentheses == '{' && charArray[i] == '}') || (closingParentheses == '[' && charArray[i] == ']'))
+                    {
+                        parentheses.Pop();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return parentheses.Count == 0 && charArrayLen != 1 ? true : false;
+        }
+    }
+}
+
